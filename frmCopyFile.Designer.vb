a512d5200da73bcd18e96c45827eee82fdc57c49
emir-karpuz <emirkarpuz.megayazilim@outlook.com>
2021-02-24 15:57:56 +0300

    Source codes added

---
+++ frmCopyFile.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmCopyFile
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtNewFiles = New System.Windows.Forms.TextBox()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtNewFiles
+        '
+        Me.txtNewFiles.Location = New System.Drawing.Point(12, 12)
+        Me.txtNewFiles.Multiline = True
+        Me.txtNewFiles.Name = "txtNewFiles"
+        Me.txtNewFiles.ReadOnly = True
+        Me.txtNewFiles.Size = New System.Drawing.Size(280, 425)
+        Me.txtNewFiles.TabIndex = 1
+        Me.txtNewFiles.TabStop = False
+        '
+        'lblResult
+        '
+        Me.lblResult.AutoSize = True
+        Me.lblResult.Location = New System.Drawing.Point(9, 440)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(44, 13)
+        Me.lblResult.TabIndex = 2
+        Me.lblResult.Text = "Sonuç :"
+        '
+        'frmCopyFile
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(304, 462)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.txtNewFiles)
+        Me.Name = "frmCopyFile"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtNewFiles As System.Windows.Forms.TextBox
+    Friend WithEvents lblResult As Label
+End Class
---
+++ frmCopyFile.vb
@@ -0,0 +1,58 @@
+﻿Imports Microsoft.VisualBasic.Strings
+'!-- MEGA TEST projesi ver:1.2 ------------------
+
+Public Class frmCopyFile
+    Dim rndNum As Integer = 0
+    Dim dosyaUzantisi As String = String.Empty
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim files() As String = IO.Directory.GetFiles(".\EXEnew\")  'Dosyaların adları alınır
+        Dim random As New Random
+        rndNum = random.Next(1000, 9999)
+
+        For Each file As String In files
+            file = dosyaAdiBul(file) & "." & dosyaUzantisi
+            If IO.File.Exists(file) Then
+                prgGuncelle(file, rndNum)
+            Else
+                My.Computer.FileSystem.CopyFile(".\EXEnew\" & file, Application.StartupPath & "\" & file, overwrite:=True)
+                txtNewFiles.Text &= ChrW(&H2714) & "Yeni Dosya Eklendi: " & file & vbCrLf
+            End If
+        Next
+
+        lblResult.Text = "İşlem Başarılı"
+    End Sub
+
+    Sub prgGuncelle(strPrgAdi, rnd)
+        Dim exeOldFileName, currentFileName As String
+
+        exeOldFileName = dosyaAdiBul(strPrgAdi) & "_" & gunHesapla() & "_" & rnd.ToString & "." & dosyaUzantisi
+        My.Computer.FileSystem.RenameFile(strPrgAdi, exeOldFileName)
+        My.Computer.FileSystem.MoveFile(Application.StartupPath & "\" & exeOldFileName, ".\EXEold\" & exeOldFileName)
+        currentFileName = Application.StartupPath & "\" & strPrgAdi
+        My.Computer.FileSystem.CopyFile(".\EXEnew\" & strPrgAdi, currentFileName, overwrite:=True)
+
+        txtNewFiles.Text += ChrW(&H2713) & "Eski Dosya Çıkarıldı: " & exeOldFileName & vbCrLf
+    End Sub
+
+    Function dosyaAdiBul(dosyaAdi As String) As String
+        Dim dosyaIlkParca As String() = Nothing
+        Dim dosyaSonParca As String() = Nothing
+        dosyaUzantisi = Strings.Right(dosyaAdi, 3)
+
+        If dosyaAdi.Contains("\EXE") Then
+            dosyaIlkParca = Split(dosyaAdi, "\")
+            dosyaSonParca = Split(dosyaIlkParca(2), ".")
+            dosyaAdi = dosyaSonParca(0)
+        Else
+            dosyaAdi = Strings.Left(dosyaAdi, InStr(dosyaAdi, ".") - 1)
+        End If
+        Return dosyaAdi
+    End Function
+
+    Function gunHesapla() As String
+        Dim gun As String = DateTime.Today.ToString("yyMMdd")
+        Return gun
+    End Function
+
+End Class
