44c75c8a403e3f5df3bfba3907219736edce68b3
emir-karpuz <emirkarpuz.megayazilim@outlook.com>
2021-02-24 17:14:25 +0300

    Try-Catch blocks added

--- frmCopyFile.Designer.vb
+++ frmCopyFile.Designer.vb
@@ -24,40 +24,98 @@ Partial Class frmCopyFile
     Private Sub InitializeComponent()
         Me.txtNewFiles = New System.Windows.Forms.TextBox()
         Me.lblResult = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.txtOldFiles = New System.Windows.Forms.TextBox()
+        Me.lblNewFiles = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'txtNewFiles
         '
-        Me.txtNewFiles.Location = New System.Drawing.Point(12, 12)
+        Me.txtNewFiles.BackColor = System.Drawing.Color.Honeydew
+        Me.txtNewFiles.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.txtNewFiles.Location = New System.Drawing.Point(12, 59)
+        Me.txtNewFiles.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.txtNewFiles.Multiline = True
         Me.txtNewFiles.Name = "txtNewFiles"
         Me.txtNewFiles.ReadOnly = True
-        Me.txtNewFiles.Size = New System.Drawing.Size(280, 425)
+        Me.txtNewFiles.Size = New System.Drawing.Size(362, 174)
         Me.txtNewFiles.TabIndex = 1
         Me.txtNewFiles.TabStop = False
         '
         'lblResult
         '
         Me.lblResult.AutoSize = True
-        Me.lblResult.Location = New System.Drawing.Point(9, 440)
+        Me.lblResult.BackColor = System.Drawing.Color.Green
+        Me.lblResult.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.lblResult.ForeColor = System.Drawing.Color.White
+        Me.lblResult.Location = New System.Drawing.Point(12, 13)
         Me.lblResult.Name = "lblResult"
-        Me.lblResult.Size = New System.Drawing.Size(44, 13)
+        Me.lblResult.Size = New System.Drawing.Size(57, 16)
         Me.lblResult.TabIndex = 2
-        Me.lblResult.Text = "Sonuç :"
+        Me.lblResult.Text = "Sonuç : "
+        '
+        'btnExit
+        '
+        Me.btnExit.BackColor = System.Drawing.Color.Red
+        Me.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnExit.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.btnExit.ForeColor = System.Drawing.Color.White
+        Me.btnExit.Location = New System.Drawing.Point(289, 13)
+        Me.btnExit.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(87, 42)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "ÇIKIŞ"
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'txtOldFiles
+        '
+        Me.txtOldFiles.BackColor = System.Drawing.Color.MistyRose
+        Me.txtOldFiles.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.txtOldFiles.Location = New System.Drawing.Point(12, 241)
+        Me.txtOldFiles.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txtOldFiles.Multiline = True
+        Me.txtOldFiles.Name = "txtOldFiles"
+        Me.txtOldFiles.ReadOnly = True
+        Me.txtOldFiles.Size = New System.Drawing.Size(362, 192)
+        Me.txtOldFiles.TabIndex = 4
+        Me.txtOldFiles.TabStop = False
+        '
+        'lblNewFiles
+        '
+        Me.lblNewFiles.AutoSize = True
+        Me.lblNewFiles.BackColor = System.Drawing.Color.Navy
+        Me.lblNewFiles.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.lblNewFiles.ForeColor = System.Drawing.Color.White
+        Me.lblNewFiles.Location = New System.Drawing.Point(12, 39)
+        Me.lblNewFiles.Name = "lblNewFiles"
+        Me.lblNewFiles.Size = New System.Drawing.Size(194, 16)
+        Me.lblNewFiles.TabIndex = 5
+        Me.lblNewFiles.Text = "Toplam EXEnew Dosya Sayısı : "
         '
         'frmCopyFile
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(304, 462)
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(388, 440)
+        Me.Controls.Add(Me.lblNewFiles)
+        Me.Controls.Add(Me.txtOldFiles)
+        Me.Controls.Add(Me.btnExit)
         Me.Controls.Add(Me.lblResult)
         Me.Controls.Add(Me.txtNewFiles)
+        Me.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Name = "frmCopyFile"
-        Me.Text = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "MEGA-Copy File Test"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
     Friend WithEvents txtNewFiles As System.Windows.Forms.TextBox
     Friend WithEvents lblResult As Label
+    Friend WithEvents btnExit As Button
+    Friend WithEvents txtOldFiles As TextBox
+    Friend WithEvents lblNewFiles As Label
 End Class
--- frmCopyFile.vb
+++ frmCopyFile.vb
@@ -4,35 +4,47 @@
 Public Class frmCopyFile
     Dim rndNum As Integer = 0
     Dim dosyaUzantisi As String = String.Empty
+    Dim toplamDosyaSayisi As Integer = 0
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
         Dim files() As String = IO.Directory.GetFiles(".\EXEnew\")  'Dosyaların adları alınır
         Dim random As New Random
         rndNum = random.Next(1000, 9999)
+        Try
+            For Each file As String In files
+                file = dosyaAdiBul(file) & "." & dosyaUzantisi
+                If IO.File.Exists(file) Then
+                    prgGuncelle(file, rndNum)
+                Else
+                    My.Computer.FileSystem.CopyFile(".\EXEnew\" & file, Application.StartupPath & "\" & file, overwrite:=True)
+                    txtNewFiles.Text += "Yeni Dosya Eklendi: " & file & vbCrLf
+                End If
+                toplamDosyaSayisi += 1
+            Next
+            lblResult.Text &= "İşlem Başarılı"
+            lblNewFiles.Text &= toplamDosyaSayisi
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Hata")
+        End Try
 
-        For Each file As String In files
-            file = dosyaAdiBul(file) & "." & dosyaUzantisi
-            If IO.File.Exists(file) Then
-                prgGuncelle(file, rndNum)
-            Else
-                My.Computer.FileSystem.CopyFile(".\EXEnew\" & file, Application.StartupPath & "\" & file, overwrite:=True)
-                txtNewFiles.Text &= ChrW(&H2714) & "Yeni Dosya Eklendi: " & file & vbCrLf
-            End If
-        Next
 
-        lblResult.Text = "İşlem Başarılı"
+
     End Sub
 
     Sub prgGuncelle(strPrgAdi, rnd)
-        Dim exeOldFileName, currentFileName As String
+        Try
+            Dim exeOldFileName, currentFileName As String
 
-        exeOldFileName = dosyaAdiBul(strPrgAdi) & "_" & gunHesapla() & "_" & rnd.ToString & "." & dosyaUzantisi
-        My.Computer.FileSystem.RenameFile(strPrgAdi, exeOldFileName)
-        My.Computer.FileSystem.MoveFile(Application.StartupPath & "\" & exeOldFileName, ".\EXEold\" & exeOldFileName)
-        currentFileName = Application.StartupPath & "\" & strPrgAdi
-        My.Computer.FileSystem.CopyFile(".\EXEnew\" & strPrgAdi, currentFileName, overwrite:=True)
+            exeOldFileName = dosyaAdiBul(strPrgAdi) & "_" & gunHesapla() & "_" & rnd.ToString & "." & dosyaUzantisi
+            My.Computer.FileSystem.RenameFile(strPrgAdi, exeOldFileName)
+            My.Computer.FileSystem.MoveFile(Application.StartupPath & "\" & exeOldFileName, ".\EXEold\" & exeOldFileName)
+            currentFileName = Application.StartupPath & "\" & strPrgAdi
+            My.Computer.FileSystem.CopyFile(".\EXEnew\" & strPrgAdi, currentFileName, overwrite:=True)
 
-        txtNewFiles.Text += ChrW(&H2713) & "Eski Dosya Çıkarıldı: " & exeOldFileName & vbCrLf
+            txtOldFiles.Text += "Eski Dosya Çıkarıldı: " & exeOldFileName & vbCrLf
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Hata")
+        End Try
     End Sub
 
     Function dosyaAdiBul(dosyaAdi As String) As String
@@ -55,4 +67,7 @@ Public Class frmCopyFile
         Return gun
     End Function
 
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Close()
+    End Sub
 End Class
